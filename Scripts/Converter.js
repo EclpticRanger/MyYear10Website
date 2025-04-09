5ad60a3ba81630da3a1641a946186ec7b4df1b85
EclpticRanger <liamthomas3887@gmail.com>
2025-04-09 14:26:04 +1200

    convert button fix

--- Scripts/Converter.js
+++ Scripts/Converter.js
@@ -1,17 +1,14 @@
 document.getElementById("convertStringButton").addEventListener("click", () => {
     const stringInput = document.getElementById("stringInput").value;
-    let ascii = "";
     let binary = "";
     let hex = "";
 
     for (let i = 0; i < stringInput.length; i++) {
         const charCode = stringInput.charCodeAt(i);
-        ascii += charCode + " ";
         binary += charCode.toString(2) + " ";
         hex += charCode.toString(16) + " ";
     }
 
-    document.getElementById("asciiOutput").textContent = `ASCII: ${ascii}`;
     document.getElementById("binaryOutput").textContent = `Binary: ${binary}`;
     document.getElementById("hexOutput").textContent = `Hexadecimal: ${hex}`;
 });
